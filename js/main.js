f6f77970f893d822a40ddbb68587069cfb2c3205
Aaron Waruszewski <Aaron.waruszewski@gmail.com>
2022-05-05 18:23:53 -0700

    Added a Fighter class

--- js/main.js
+++ js/main.js
@@ -6,4 +6,23 @@ class Character{
         this.equipment=equipment
         this.level=level
     }
+
+    rollDice(){
+        return Math.floor(Math.random() * 20)
+    }
+
+    attack(target, attackType){}
+}
+
+class Fighter extends Character {
+    constructor(name, race, stats, equipment, level, fightingStyle, range){
+        super(name, race, stats, equipment, level);
+
+        this.fightingStyle = fightingStyle;
+        this.range = range;
+    }
+
+    attack(taget, attackType){
+        // Attack target with attack type.
+    }
 }
