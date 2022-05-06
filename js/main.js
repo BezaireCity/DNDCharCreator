b602256a87105593c9420ffb3f9bfb2f6548aa3b
Dallas Bezaire <102712130+BezaireCity@users.noreply.github.com>
2022-05-05 19:10:00 -0600

    added abstraction of various properties

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 class Character{
     constructor(name, race, stats, equipment, level){
-        this.name=name
-        this.race=race
-        this.stats=stats
+        this._name=name
+        this._race=race
+        this._stats=stats
         this.equipment=equipment
-        this.level=level
+        this._level=level
+    }
+    get name(_name){
+        return _name
+    }
+    get race(_race){
+        return _race
+    }
+    get stats(_stats){
+        return _stats
+    }
+    get level(_level){
+        return _level
     }
 }
